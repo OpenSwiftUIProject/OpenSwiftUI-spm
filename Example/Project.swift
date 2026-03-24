832086bee70cf2f1666a74c27f292753b6d11367
Kyle <kyle201817146@gmail.com>
2026-03-25 01:47:10 +0800

    Add UILaunchScreen to Info.plist for modern full-screen layout

--- Example/Project.swift
+++ Example/Project.swift
@@ -7,8 +7,13 @@ let project = Project(
             name: "Example",
             destinations: .iOS,
             product: .app,
-            bundleId: "org.OpenSwiftUIProject.Example",
+            bundleId: "org.OpenSwiftUIProject.OpenSwiftUI-binary.Example",
             deploymentTargets: .iOS("18.0"),
+            infoPlist: .extendingDefault(with: [
+                "UILaunchScreen": [
+                    "UIColorName": "",
+                ],
+            ]),
             sources: ["Sources/**"],
             dependencies: [
                 .external(name: "OpenSwiftUI"),
